1f11da7f5794a194f50fb2ac70404554e9a1fe2d
unknown <jones.bryson428@gmail.cogit config user.email>
2022-02-20 13:15:57 -0500

    02/19/22edit

---
+++ script.js
@@ -0,0 +1,12 @@
+function ebukasite () {
+    window.location.assign("ebuka.html");
+
+}
+function brysonsite () {
+    window.location.assign("bryson.html");
+
+}
+function tamarasite () {
+    window.location.assign("tamara.html");
+
+}
